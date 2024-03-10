93069d83d65304adf8477bda3a238249c672b51c
Abdul Rahman <fz050799@gmail.com>
2024-03-10 14:14:39 +0400

    dateY

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -3,7 +3,7 @@
      <div class="container-fluid">
          <div class="row">
              <div class="col-md-12 footer-copyright text-center">
-                 <p class="mb-0">Copyright 2022 © Ansee Nails Care </p>
+                 <p class="mb-0">Copyright {{date('Y')}} © Ansee Nails Care </p>
              </div>
          </div>
      </div>
