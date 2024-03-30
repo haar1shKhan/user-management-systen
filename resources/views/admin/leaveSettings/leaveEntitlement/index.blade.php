1586dff1a8750ee8497ac49f1e1f0844d3f120a5
Abdul Rahman <fz050799@gmail.com>
2024-03-30 05:07:56 +0400

    ds

--- resources/views/admin/leaveSettings/leaveEntitlement/index.blade.php
+++ resources/views/admin/leaveSettings/leaveEntitlement/index.blade.php
@@ -67,7 +67,7 @@
                             </div>
                         </div>
                     </div>
-                    <p class="text-danger">*Leave year will be counted from User's joining date.</p>
+                    <p class="text-danger">*سيتم احتساب سنة العمل من تاريخ تعيين الموظف.</p>
                 </div>
         </div>
 
